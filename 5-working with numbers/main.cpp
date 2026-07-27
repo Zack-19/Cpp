0948c3e030195296e625e0c4e5bd4b9192614a8a
Zack-19 <jz_safa@esi.dz>
2026-07-27 13:17:27 +0100

    working with numbers

---
+++ 5-working with numbers/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    
+    cout<<7<<endl;
+    cout<<17.25125<<endl;
+    cout<<7+5<<endl;
+    cout<<7-5<<endl;
+    cout<<7/5<<endl;
+    cout<<7*5<<endl;
+    cout<<7%5<<endl;
+    int num=7;
+    double dnum = 7.5;
+    num++;
+    dnum--;
+    cout<<num<<endl;
+    cout<<dnum<<endl;
+    num+=50;
+    cout<<num<<endl;
+    num*=2;
+    cout<<num<<endl;
+
+    cout<<7/5<<endl;
+    cout<<7.0/5<<endl;
+    cout<<pow(2,5)<<endl;
+    cout<<sqrt(36)<<endl;
+    cout<<round(5.1)<<endl;
+    cout<<ceil(5.1)<<endl;
+    cout<<floor(5.9)<<endl;
+    cout<<fmax(5,9)<<endl;
+    cout<<fmin(5,9)<<endl;
+
+    return 0;
+}
