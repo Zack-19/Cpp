2d6d8ef53e7ffbe4a20342787a7540058b066de9
Zack-19 <jz_safa@esi.dz>
2026-07-27 13:15:13 +0100

    data types

---
+++ 3-data types/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char grade ='A';
+    string name = "luc graham";
+    int age =23;
+    double gpa =3.5;
+    bool isMale = true;
+
+    cout<<gpa<<endl;
+    return 0;
+}
