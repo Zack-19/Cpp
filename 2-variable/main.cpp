9437a92dff1784fea62c2866604e9cc0aeea55fa
Zack-19 <jz_safa@esi.dz>
2026-07-27 13:14:06 +0100

    variable

---
+++ 2-variable/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string characterName ="john";
+    int characterAge;
+    characterAge=35;
+    cout<< "There once was a man named " << characterName<<endl;
+    cout<< "he was "<<characterAge<<" old" <<endl;
+    characterName="mike";
+    cout<< "he liked the name "<< characterName <<endl;
+    cout<< "but did not like being "<< characterAge <<endl;
+
+    return 0;
+}
