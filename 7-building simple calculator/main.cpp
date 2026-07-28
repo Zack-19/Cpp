fd273c22eb8cec503310ae775e025b6bce7b0bb9
Zack-19 <jz_safa@esi.dz>
2026-07-28 09:07:16 +0100

    building calculator v1

---
+++ 7-building simple calculator/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double num1,num2;
+    cout<<"enter 1st number : ";
+    cin>>num1;
+    cout<<"enter 2nd number: ";
+    cin>>num2;
+    cout<<num1+num2;
+
+
+
+    return 0;
+}
