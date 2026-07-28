2d3f7dd930551d51ee731cc275976b131b731a00
Zack-19 <jz_safa@esi.dz>
2026-07-28 09:06:12 +0100

    getting user input

---
+++ 6-getting user input/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    
+   /*int age;
+   cout<< "enter your age : ";
+   cin >> age;
+   cout<<"you are "<< age<< " years old";*/
+
+   string name;
+   cout<<"enter your name : ";
+   getline(cin,name);
+   cout<<"hello! "<<name ;
+
+
+
+    return 0;
+}
