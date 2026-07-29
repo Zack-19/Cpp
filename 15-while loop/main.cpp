555d96938790a8e0e59e08933c3541b05875bce8
Zack-19 <jz_safa@esi.dz>
2026-07-29 22:25:20 +0100

    while loop

---
+++ 15-while loop/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+
+int main(){
+    
+    int index = 1 ;
+    while (index <=5)
+    {
+        cout<<index<<endl;
+        index++;
+    }
+    
+    index =6;
+
+    do{
+        cout<<"do while "<<index<<endl;
+        index++;
+    }while (index<=5);
+    
+
+    return 0;
+}
