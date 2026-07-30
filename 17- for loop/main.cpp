14c6e4b87611b14d989fb7b16de74a07758ec3a3
Zack-19 <jz_safa@esi.dz>
2026-07-30 20:17:52 +0100

    FOR LOOP

---
+++ 17- for loop/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+
+int main(){
+    
+    for (int i = 1; i <= 5; i++)
+    {
+        cout<<i<<endl;
+    }
+
+    int nums[]={0,1,17,184,12,91,7};
+
+    for (int i = 0; i < 7 ; i++)
+    {
+        cout<<nums[i]<<endl;
+    }
+    
+    
+
+    return 0;
+}
